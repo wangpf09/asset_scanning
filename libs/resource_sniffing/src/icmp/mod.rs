e568a17685c8e5e99abd710eaca41e7e371b16a2
wangpf <wangpf09@126.com>
2023-11-24 17:54:42 +0800

    parse all ip

--- libs/resource_sniffing/src/icmp/mod.rs
+++ libs/resource_sniffing/src/icmp/mod.rs
@@ -1,12 +1,14 @@
 mod icmp_sniffing;
 
 
-pub fn parse_cidr(cidrs: Vec<String>) {
+pub fn parse_cidr(cidrs: Vec<String>) -> Vec<String> {
+    let mut ips: Vec<String> = Vec::new();
     for ci in cidrs {
         if ci.contains("/") {
-            icmp_sniffing::parse_cidr_ipv4(ci);
+            ips.extend(icmp_sniffing::parse_cidr_ipv4(ci));
         } else if ci.contains("-") {
-            icmp_sniffing::parse_ipv4_range(ci);
+            ips.extend(icmp_sniffing::parse_ipv4_range(ci));
         }
     }
+    return ips;
 }
--- libs/resource_sniffing/src/lib.rs
+++ libs/resource_sniffing/src/lib.rs
@@ -4,6 +4,8 @@ pub mod icmp;
 mod tests {
     use std::env;
 
+    use log::debug;
+
     use super::*;
 
     #[test]
@@ -12,8 +14,11 @@ mod tests {
         env_logger::init();
         let cidrs = vec![
             String::from("192.168.0.1/24"),
-            String::from("127.0.0.1-127.0.0.15")
+            String::from("127.0.0.1-127.0.0.15"),
         ];
-        icmp::parse_cidr(cidrs)
+        let ips = icmp::parse_cidr(cidrs);
+        for ip in ips {
+            debug!("ip {}", ip)
+        }
     }
 }
